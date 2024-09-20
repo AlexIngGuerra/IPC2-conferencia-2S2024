fa9dfab728b2e0625e6b57babb7154ebc9cbfe0e
Walter Alexander Guerra Duque <3018766650101@ingenieria.usac.edu.gt>
2024-09-20 13:46:43 -0600

    Agregar generar contenido Dot

--- utils/estructuras/listaDoble.py
+++ utils/estructuras/listaDoble.py
@@ -24,3 +24,28 @@ class ListaDoble:
         
         aux.siguiente = nuevo
         nuevo.anterior = aux
+
+   # Generar grafica en dot
+    def getDot(self):
+        if(self.inicio == None):
+            print("### Error: Lista vacia")
+            return ""
+        
+        idNodo = 0
+        cadena = "digraph G {\nnode[shape=record];\nrankdir=\"LR\";\n"
+
+        idNodo += 1
+        dotNode = "{} [label=\"{}\"];\n".format(idNodo, self.inicio.data)
+        cadena = cadena + dotNode
+
+        aux = self.inicio.siguiente
+        while aux != None:
+            idNodo += 1
+            dotNode = "{} [label=\"{}\"];\n".format(idNodo, aux.data)
+            dotNext = "{}->{};\n".format(idNodo-1, idNodo)
+            dotPrev= "{}->{};\n".format(idNodo, idNodo-1)
+            cadena = cadena + dotNode + dotNext + dotPrev
+            aux = aux.siguiente
+
+        cadena = cadena + "\n}"
+        return cadena
