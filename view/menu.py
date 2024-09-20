9256db8b4dc781674e8a0997d2be4f330fcb51f1
Walter Alexander Guerra Duque <3018766650101@ingenieria.usac.edu.gt>
2024-09-20 13:09:37 -0600

    Agregar menu principal

---
+++ view/menu.py
@@ -0,0 +1,18 @@
+from os import system
+
+def menu():
+    while True:
+        system("cls")
+        print("########### MENU PRINCIPAL ############")
+        print("1. Graficar Lista Simple")
+        print("2. Graficar Lista Doble")
+        print("3. Salir")
+
+        opcion = input()
+        if opcion == "1":
+            print("Graficando lista simple")
+        elif opcion == "2":
+            print("Graficando lista doble")
+        elif opcion == "3":
+            break
+        input()
